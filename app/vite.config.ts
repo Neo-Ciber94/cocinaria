499fcb840be609d74a5393e61428afe182086fd9
Freddy L <neo-ciber_94@hotmail.com>
2024-07-11 22:23:29 -0400

    Update downgrade vite

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,8 +1,8 @@
 import 'dotenv/config';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+
 export default defineConfig({
-	// @ts-expect-error Types mismatch
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
