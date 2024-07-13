5239e6c0f3ed064a779718544cfc634b2b73338f
Freddy L <neo-ciber_94@hotmail.com>
2024-07-12 20:56:02 -0400

    Update icon

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -4,6 +4,9 @@
 	import { Toaster } from 'svelte-french-toast';
 	import UserAvatar from './UserAvatar.svelte';
 
+	const FOOD_ICONS = ['🍕', '🍔', '🧁', '🍰', '🍊', '🍒', '🍇', '🥕', '🥦'];
+	const icon = FOOD_ICONS[Math.floor(Math.random() % FOOD_ICONS.length)];
+
 	export let data: LayoutData;
 </script>
 
@@ -14,9 +17,10 @@
 <header
 	class="flex flex-row p-4 items-center justify-between w-full min-h-16 shadow-lg border-b border-gray-200"
 >
-	<h1 class="text-2xl font-bold">
+	<h1 class="text-2xl font-bold flex flex-row font-mono text-orange-500">
 		<span>Cociner</span>
 		<span>IA</span>
+		<span>{icon}</span>
 	</h1>
 
 	{#if data.user}
