789252130dc7ad96382feb3f9727e4fbb90341e6
Freddy L <neo-ciber_94@hotmail.com>
2024-07-13 02:00:13 -0400

    Update title

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -31,7 +31,7 @@
 <header class="flex flex-row p-4 items-center justify-between w-full h-[var(--header-height)]">
 	<a class="text-2xl font-bold flex flex-row justify-center font-mono text-orange-500" href="/">
 		<span>Cocinar</span>
-		<span>IA</span>
+		<span class="text-emerald-500">IA</span>
 		<span>{icon}</span>
 	</a>
 
