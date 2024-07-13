ab164426d991edd65e49e75df4aa64e57f8fa9e6
Freddy L <neo-ciber_94@hotmail.com>
2024-07-13 16:28:17 -0400

    Update home page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
+	import SushiIcon from '$components/icons/sushiIcon.svelte';
 	import type { LayoutData } from './$types';
 
 	export let data: LayoutData;
 	const user = data.auth?.user;
 </script>
 
-<div class="w-full h-full flex flex-col justify-center items-center">
-	{#if user}
-		<h1>Username: {user.username}</h1>
-		<a href="/api/auth/logout">Logout</a>
-	{:else}
-		<a href="/login" class="text-2xl font-bold">Go to Login</a>
-	{/if}
+<div class="w-full h-full flex flex-col justify-center items-center mx-auto md:max-w-3xl">
+	<section class="flex flex-row gap-4 items-center">
+		<SushiIcon class="w-[300px] sm:w-[30%] max-w-[500px] aspect-square" />
+		<h3 class="text-lg sm:text-3xl md:text-5xl font-bold">
+			Turn Ingredients into Delicious Dishes!
+		</h3>
+	</section>
 </div>
