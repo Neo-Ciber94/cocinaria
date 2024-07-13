1cffcf64b59a33889ae9c778b09c280839ddecdf
Freddy L <neo-ciber_94@hotmail.com>
2024-07-13 12:59:38 -0400

    Update CI

--- app/src/lib/auth/lucia.ts
+++ app/src/lib/auth/lucia.ts
@@ -3,7 +3,16 @@ import { sessions, users } from '$lib/db/schema';
 import { DrizzlePostgreSQLAdapter } from '@lucia-auth/adapter-drizzle';
 import { Discord, GitHub, Google } from 'arctic';
 import { Lucia, TimeSpan } from 'lucia';
-import { env } from '$env/dynamic/private';
+import {
+	GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET,
+	GOOGLE_REDIRECT_URL,
+	GITHUB_CLIENT_ID,
+	GITHUB_CLIENT_SECRET,
+	DISCORD_CLIENT_ID,
+	DISCORD_CLIENT_SECRET,
+	DISCORD_REDIRECT_URL
+} from '$env/static/private';
 
 const adapter = new DrizzlePostgreSQLAdapter(db, sessions, users);
 
@@ -34,16 +43,12 @@ declare module 'lucia' {
 	}
 }
 
-export const googleAuth = new Google(
-	env.GOOGLE_CLIENT_ID!,
-	env.GOOGLE_CLIENT_SECRET!,
-	env.GOOGLE_REDIRECT_URL!
-);
+export const googleAuth = new Google(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URL);
 
-export const githubAuth = new GitHub(env.GITHUB_CLIENT_ID!, env.GITHUB_CLIENT_SECRET!);
+export const githubAuth = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET);
 
 export const discordAuth = new Discord(
-	env.DISCORD_CLIENT_ID!,
-	env.DISCORD_CLIENT_SECRET!,
-	env.DISCORD_REDIRECT_URL!
+	DISCORD_CLIENT_ID,
+	DISCORD_CLIENT_SECRET,
+	DISCORD_REDIRECT_URL
 );
