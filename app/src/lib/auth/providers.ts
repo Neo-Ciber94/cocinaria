35a215131acf9e027ddf3ae9e65c178f6bc457a1
Freddy L <neo-ciber_94@hotmail.com>
2024-07-13 13:17:43 -0400

    minor changes

--- app/src/lib/auth/providers.ts
+++ app/src/lib/auth/providers.ts
@@ -2,15 +2,15 @@ import { Discord, GitHub, Google } from 'arctic';
 import { env } from '$env/dynamic/private';
 
 export const googleAuth = new Google(
-	env.GOOGLE_CLIENT_ID,
-	env.GOOGLE_CLIENT_SECRET,
-	env.GOOGLE_REDIRECT_URL
+	env.GOOGLE_CLIENT_ID!,
+	env.GOOGLE_CLIENT_SECRET!,
+	env.GOOGLE_REDIRECT_URL!
 );
 
-export const githubAuth = new GitHub(env.GITHUB_CLIENT_ID, env.GITHUB_CLIENT_SECRET);
+export const githubAuth = new GitHub(env.GITHUB_CLIENT_ID!, env.GITHUB_CLIENT_SECRET!);
 
 export const discordAuth = new Discord(
-	env.DISCORD_CLIENT_ID,
-	env.DISCORD_CLIENT_SECRET,
-	env.DISCORD_REDIRECT_URL
+	env.DISCORD_CLIENT_ID!,
+	env.DISCORD_CLIENT_SECRET!,
+	env.DISCORD_REDIRECT_URL!
 );
