c62cf908286cd55c59e87a3496c28704d4f870e4
Freddy L <neo-ciber_94@hotmail.com>
2024-07-13 02:31:07 -0400

    ignore null

--- app/src/lib/auth/lucia.ts
+++ app/src/lib/auth/lucia.ts
@@ -35,15 +35,15 @@ declare module 'lucia' {
 }
 
 export const googleAuth = new Google(
-	env.GOOGLE_CLIENT_ID,
-	env.GOOGLE_CLIENT_SECRET,
-	env.GOOGLE_REDIRECT_URL
+	env.GOOGLE_CLIENT_ID!,
+	env.GOOGLE_CLIENT_SECRET!,
+	env.GOOGLE_REDIRECT_URL!
 );
 
-export const githubAuth = new GitHub(env.GITHUB_CLIENT_ID, env.GITHUB_CLIENT_SECRET);
+export const githubAuth = new GitHub(env.GITHUB_CLIENT_ID!, env.GITHUB_CLIENT_SECRET!);
 
 export const discordAuth = new Discord(
-	env.DISCORD_CLIENT_ID,
-	env.DISCORD_CLIENT_SECRET,
-	env.DISCORD_REDIRECT_URL
+	env.DISCORD_CLIENT_ID!,
+	env.DISCORD_CLIENT_SECRET!,
+	env.DISCORD_REDIRECT_URL!
 );
