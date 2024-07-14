5d451f8bce7bc35145068769c09eeba1edbee93d
Freddy L <neo-ciber_94@hotmail.com>
2024-07-13 20:37:26 -0400

    Add ingredient selector

--- app/src/routes/generate/+page.svelte
+++ app/src/routes/generate/+page.svelte
@@ -1,1 +1,91 @@
-<h1>Generate</h1>
+<script lang="ts">
+	import SparkIcon from '$components/icons/sparkIcon.svelte';
+	import { Button } from 'bits-ui';
+	import { INGREDIENTS, type Ingredient } from './ingredients';
+	import IngredientSelect from './IngredientSelect.svelte';
+	import { flip } from 'svelte/animate';
+	import { quintOut } from 'svelte/easing';
+	import { fly } from 'svelte/transition';
+	import { fade } from 'svelte/transition';
+
+	type Item = {
+		id: string;
+		ingredient: Ingredient | undefined;
+	};
+
+	const MIN_INGREDIENTS = 2;
+	const MAX_INGREDIENTS = 10;
+
+	let selectedItems = $state<Item[]>([]);
+
+	let ingredients = $derived.by(() => {
+		const selectedIngredients = selectedItems
+			.map((s) => s.ingredient)
+			.filter(Boolean) as Ingredient[];
+		return INGREDIENTS.filter((ingredient) => {
+			const isAlreadyAdded = selectedIngredients.some((e) => e.value === ingredient.value);
+			return !isAlreadyAdded;
+		});
+	});
+
+	function addIngredient() {
+		if (selectedItems.length === MAX_INGREDIENTS) {
+			return;
+		}
+
+		selectedItems.push({ ingredient: undefined, id: crypto.randomUUID() });
+	}
+
+	function removeIngredient(id: string) {
+		selectedItems = selectedItems.filter((ingredient) => ingredient.id !== id);
+	}
+
+	function handleChangeIngredient(id: string, ingredient: Ingredient | undefined) {
+		selectedItems = selectedItems.map((item) => {
+			if (item.id === id) {
+				return { ...item, ingredient };
+			}
+
+			return item;
+		});
+	}
+</script>
+
+<div class="p-4 container mx-auto w-full h-full flex flex-col gap-2 max-w-xl">
+	<h1 class="flex flex-row gap-2 mx-auto text-2xl sm:text-4xl items-center text-orange-400">
+		<SparkIcon class="size-8 sm:size-12" />
+		<span>Generate Recipe</span>
+	</h1>
+
+	<div class="w-full mx-auto mt-10 flex flex-col items-center gap-2">
+		<h2 class="font-bold font-mono text-xl">Ingredients</h2>
+
+		{#each selectedItems as item (item.id)}
+			<div
+				class="flex flex-row items-center gap-2 w-full"
+				transition:fly={{
+					duration: 300,
+					x: -100,
+					opacity: 0.1,
+					easing: quintOut
+				}}
+			>
+				<IngredientSelect
+					class="w-full"
+					{ingredients}
+					onchange={(ingredient) => handleChangeIngredient(item.id, ingredient)}
+				/>
+
+				<button
+					class="bg-red-500 text-white p-2 rounded-md"
+					onclick={() => removeIngredient(item.id)}>Delete</button
+				>
+			</div>
+		{/each}
+
+		<Button.Root
+			onclick={addIngredient}
+			class="rounded-lg px-4 py-2 bg-orange-500 text-white w-full">Add Ingredient</Button.Root
+		>
+	</div>
+</div>
---
+++ app/src/routes/generate/IngredientSelect.svelte
@@ -0,0 +1,101 @@
+<script lang="ts">
+	import { Combobox, type Selected } from 'bits-ui';
+	import { flyAndScale } from '$lib/utils/transitions';
+	import Icon from '@iconify/svelte';
+	import { type Ingredient } from './ingredients';
+	import { cn } from '$lib';
+
+	let inputValue = $state('');
+	let touchedInput = $state(false);
+
+	type Props = {
+		class?: string;
+		inputClass?: string;
+		contentClass?: string;
+		ingredients: Ingredient[];
+		onchange: (item: Ingredient | undefined) => void;
+	};
+
+	let { onchange, ingredients, ...rest }: Props = $props();
+
+	let selectedIngredient = $state<Ingredient>();
+	const initialIngredients = $state<Ingredient[]>(ingredients);
+	const filteredIngredients = $derived.by(() => {
+		return initialIngredients.filter((ingredient) =>
+			ingredient.value.includes(inputValue.toLowerCase())
+		);
+	});
+
+	function handleChange(selected: Selected<string> | undefined) {
+		if (selected) {
+			const ingredient = filteredIngredients.find((x) => x.value === selected.value);
+			selectedIngredient = ingredient;
+			onchange(ingredient);
+		} else {
+			selectedIngredient = undefined;
+			onchange(undefined);
+		}
+	}
+</script>
+
+<Combobox.Root
+	items={filteredIngredients}
+	bind:inputValue
+	bind:touchedInput
+	onSelectedChange={handleChange}
+>
+	<div class={cn('relative', rest.class)}>
+		{#if selectedIngredient}
+			<div class="absolute start-3 top-1/2 text-lg size-6 -translate-y-1/2">
+				{selectedIngredient.image}
+			</div>
+		{:else}
+			<Icon
+				icon="ph:orange-slice"
+				class="absolute start-3 top-1/2 size-6 -translate-y-1/2 text-neutral-500"
+			/>
+		{/if}
+
+		<Combobox.Input
+			class={cn(
+				'inline-flex h-input w-full truncate rounded-lg border border-gray-200 bg-white px-11 py-2 text-sm transition-colors focus:outline-none focus:ring-2 focus:ring-foreground focus:ring-offset-2 focus:ring-offset-white',
+				rest.inputClass
+			)}
+			placeholder="Search a fruit"
+			aria-label="Search a fruit"
+		/>
+		<Icon
+			icon="heroicons:chevron-up-down-16-solid"
+			class="absolute end-3 top-1/2 size-6 -translate-y-1/2"
+		/>
+	</div>
+
+	<Combobox.Content
+		class={cn(
+			'w-full rounded-xl border border-neutral-300 bg-white px-1 py-3 shadow-sm outline-none max-h-[500px] overflow-y-auto',
+			rest.contentClass
+		)}
+		transition={flyAndScale}
+		sideOffset={8}
+	>
+		{#each filteredIngredients as ingredient (ingredient.value)}
+			<Combobox.Item
+				class="flex h-10 w-full select-none items-center rounded-md py-3 pl-5 pr-1.5 text-sm sm:text-lg outline-none transition-all gap-1 duration-75 data-[highlighted]:bg-neutral-200"
+				value={ingredient.value}
+				label={ingredient.value}
+			>
+				{ingredient.image}
+
+				<span class="capitalize">
+					{ingredient.value}
+				</span>
+				<Combobox.ItemIndicator class="ml-auto" asChild={false}>
+					<Icon icon="material-symbols:check" />
+				</Combobox.ItemIndicator>
+			</Combobox.Item>
+		{:else}
+			<span class="block px-5 py-2 text-sm text-neutral-300"> No ingredient found </span>
+		{/each}
+	</Combobox.Content>
+	<Combobox.HiddenInput name="ingredient" />
+</Combobox.Root>
