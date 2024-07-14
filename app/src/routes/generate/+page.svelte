97872517c79e8be4291ec8bc42cfb763ac1683b2
Freddy L <neo-ciber_94@hotmail.com>
2024-07-14 11:36:17 -0400

    Add card in generate recipe

--- app/src/routes/generate/+page.svelte
+++ app/src/routes/generate/+page.svelte
@@ -8,6 +8,7 @@
 	import { useRecipeItems, MIN_INGREDIENTS, MAX_INGREDIENTS } from './useRecipeItems.svelte';
 	import LoadingIcon from '$components/icons/loadingIcon.svelte';
 	import { cn, delay } from '$lib';
+	import RecipeLoading from './RecipeLoading.svelte';
 
 	const recipeItems = useRecipeItems();
 	const selectedCount = $derived.by(() => {
@@ -32,113 +33,125 @@
 				throw new Error('Generation failed');
 			}
 
-			await delay(2000);
+			await delay(50000);
 		} finally {
 			isGenerating = false;
 		}
 	}
+
+	const ingredientImges = $derived.by(() => {
+		return recipeItems.selectedItems
+			.filter((e) => Boolean(e.ingredient))
+			.map((e) => e.ingredient?.image) as string[];
+	});
 </script>
 
-<div class="p-4 container mx-auto w-full h-full flex flex-col gap-2 max-w-xl pt-10 sm:pt-20">
-	<h1 class="flex flex-row gap-2 mx-auto text-2xl sm:text-4xl items-center text-orange-400">
-		<SparkIcon class="size-8 sm:size-12" animated />
-		<span>Generate Recipe</span>
-	</h1>
+<div class="p-4 container mx-auto w-full h-full max-w-xl pt-10 sm:pt-20">
+	<div class="px-4 pt-8 pb-6 flex flex-col gap-2 shadow-md rounded-xl border border-gray-200">
+		<h1 class="flex flex-row gap-2 mx-auto text-2xl sm:text-4xl items-center text-orange-400">
+			<SparkIcon class="size-8 sm:size-12" animated />
+			<span>Generate Recipe</span>
+		</h1>
 
-	<p class="p-4 rounded-lg bg-orange-100 shadow-sm text-center">
-		Select from <strong>2</strong> up to <strong>10</strong> to generate a new recipe using our AI
-	</p>
+		<p class="p-4 rounded-lg bg-orange-100 shadow-sm text-center">
+			Select from <strong>2</strong> up to <strong>10</strong> to generate a new recipe using our AI
+		</p>
 
-	<div class="w-full mx-auto mt-10 flex flex-col items-center gap-2">
-		<h2 class="font-bold font-mono text-xl">Ingredients</h2>
+		<div class="w-full mx-auto mt-10 flex flex-col items-center gap-2">
+			<h2 class="font-bold font-mono text-xl">Ingredients</h2>
 
-		{#if selectedCount > 0}
-			<div
-				class="w-full"
-				transition:scale={{ duration: 300, opacity: 0.2, start: 0.8, easing: quintOut }}
-			>
-				<AmountIndicator min={MIN_INGREDIENTS} max={MAX_INGREDIENTS} count={selectedCount} />
-			</div>
-		{/if}
-
-		{#each recipeItems.selectedItems as item (item.id)}
-			<div
-				class="flex flex-row items-center gap-2 w-full"
-				transition:fly={{
-					duration: 300,
-					x: -100,
-					opacity: 0.1,
-					easing: quintOut
-				}}
-			>
-				<IngredientSelect
+			{#if selectedCount > 0}
+				<div
 					class="w-full"
-					disabled={isGenerating}
-					ingredients={recipeItems.ingredients}
-					selectedIngredient={item.ingredient}
-					onchange={(ingredient) => recipeItems.update(item.id, ingredient)}
-				/>
+					transition:scale={{ duration: 300, opacity: 0.2, start: 0.8, easing: quintOut }}
+				>
+					<AmountIndicator min={MIN_INGREDIENTS} max={MAX_INGREDIENTS} count={selectedCount} />
+				</div>
+			{/if}
+
+			{#if isGenerating}
+				<RecipeLoading images={ingredientImges} />
+			{/if}
+
+			{#each recipeItems.selectedItems as item (item.id)}
+				<div
+					class="flex flex-row items-center gap-2 w-full"
+					transition:fly={{
+						duration: 300,
+						x: -100,
+						opacity: 0.1,
+						easing: quintOut
+					}}
+				>
+					<IngredientSelect
+						class="w-full"
+						disabled={isGenerating}
+						ingredients={recipeItems.ingredients}
+						selectedIngredient={item.ingredient}
+						onchange={(ingredient) => recipeItems.update(item.id, ingredient)}
+					/>
+
+					<Button.Root
+						disabled={isGenerating}
+						class={cn(
+							`bg-red-500 text-white p-2 rounded-md`,
+							isGenerating ? '' : 'hover:bg-red-600',
+							'disabled:opacity-70 disabled:cursor-not-allowed'
+						)}
+						onclick={() => recipeItems.remove(item.id)}>Remove</Button.Root
+					>
+				</div>
+			{/each}
+
+			<div class="w-full flex sm:flex-row flex-col justify-between gap-2 mt-2">
+				<Button.Root
+					disabled={!canGenerate || isGenerating}
+					onclick={generateRecipe}
+					class={cn(
+						`relative rounded-lg px-4 py-2 justify-center text-white w-full flex flex-row items-center gap-1 bg-neutral-700`,
+						!canGenerate || isGenerating ? '' : 'hover:bg-neutral-800',
+						'disabled:opacity-70 disabled:cursor-not-allowed'
+					)}
+				>
+					{#if isGenerating}
+						<LoadingIcon class="text-white size-6" />
+						<span> Generating</span>
+					{:else}
+						<SparkIcon class="size-6" />
+						<span> Generate</span>
+					{/if}
+				</Button.Root>
 
 				<Button.Root
 					disabled={isGenerating}
+					onclick={recipeItems.add}
 					class={cn(
-						`bg-red-500 text-white p-2 rounded-md`,
-						isGenerating ? '' : 'hover:bg-red-600',
-						'disabled:opacity-70 disabled:cursor-not-allowed'
+						'relative rounded-lg px-4 py-2 bg-orange-500 justify-center text-white w-full flex flex-row items-center gap-1',
+						isGenerating ? '' : 'hover:bg-orange-600',
+						'disabled:opacity-70 disabled:cursor-not-allowed',
+						{
+							'animate-pulse': recipeItems.pending
+						}
 					)}
-					onclick={() => recipeItems.remove(item.id)}>Remove</Button.Root
 				>
+					{#if recipeItems.pending}
+						<LoadingIcon class="text-white size-6" />
+					{:else}
+						<svg xmlns="http://www.w3.org/2000/svg" class="text-white size-6" viewBox="0 0 24 24">
+							<path
+								fill="none"
+								stroke="currentColor"
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								stroke-width="2"
+								d="M5 12h14m-7-7v14"
+							/>
+						</svg>
+					{/if}
+
+					<span> Add Ingredient </span>
+				</Button.Root>
 			</div>
-		{/each}
-
-		<div class="w-full flex flex-row justify-between gap-2 mt-2">
-			<Button.Root
-				disabled={!canGenerate || isGenerating}
-				onclick={generateRecipe}
-				class={cn(
-					`relative rounded-lg px-4 py-2 justify-center text-white w-full flex flex-row items-center gap-1 bg-neutral-700`,
-					!canGenerate || isGenerating ? '' : 'hover:bg-neutral-800',
-					'disabled:opacity-70 disabled:cursor-not-allowed'
-				)}
-			>
-				{#if isGenerating}
-					<LoadingIcon class="text-white size-6" />
-					<span> Generating</span>
-				{:else}
-					<SparkIcon class="size-6" />
-					<span> Generate</span>
-				{/if}
-			</Button.Root>
-
-			<Button.Root
-				disabled={isGenerating}
-				onclick={recipeItems.add}
-				class={cn(
-					'relative rounded-lg px-4 py-2 bg-orange-500 justify-center text-white w-full flex flex-row items-center gap-1',
-					isGenerating ? '' : 'hover:bg-orange-600',
-					'disabled:opacity-70 disabled:cursor-not-allowed',
-					{
-						'animate-pulse': recipeItems.pending
-					}
-				)}
-			>
-				{#if recipeItems.pending}
-					<LoadingIcon class="text-white size-6" />
-				{:else}
-					<svg xmlns="http://www.w3.org/2000/svg" class="text-white size-6" viewBox="0 0 24 24">
-						<path
-							fill="none"
-							stroke="currentColor"
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M5 12h14m-7-7v14"
-						/>
-					</svg>
-				{/if}
-
-				<span> Add Ingredient </span>
-			</Button.Root>
 		</div>
 	</div>
 </div>
