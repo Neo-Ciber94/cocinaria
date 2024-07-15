58c05cb9515640e5411e6fa63aac770a07d2d9e0
Freddy L <neo-ciber_94@hotmail.com>
2024-07-14 20:14:44 -0400

    Update my recipes page

--- app/src/routes/my_recipes/+page.svelte
+++ app/src/routes/my_recipes/+page.svelte
@@ -25,18 +25,20 @@
 	}
 </script>
 
-<div class="p-4 container mx-auto w-full h-full max-w-3xl lg:max-w-5xl pt-10 sm:pt-20">
+<div class="p-4 container mx-auto w-full h-full max-w-4xl lg:max-w-6xl pt-10 sm:pt-20">
 	<h1 class="flex flex-row gap-2 mx-auto text-2xl sm:text-4xl items-center text-orange-400">
 		<CookingIcon class="size-8 sm:size-12" />
 		<span>My Recipes</span>
 	</h1>
 
-	<div class="flex flex-row gap-4 flex-wrap py-5 justify-center">
+	<div
+		class="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 flex-wrap py-5 justify-center"
+	>
 		{#if hasRecipes}
 			{#each recipes as recipe, index}
 				<a
 					href={`/recipes/${recipe.id}`}
-					class="flex flex-col gap-1 p-2 rounded-lg shadow-md border border-gray-200 w-[180px] md:w-[220px] xl:w-[300px] bg-white"
+					class="flex flex-col gap-1 p-2 rounded-lg shadow-md border border-gray-200 w-full bg-white"
 					in:scale|local={{
 						duration: 500,
 						opacity: 0.01,
@@ -45,18 +47,15 @@
 						delay: index * 100
 					}}
 				>
-					<img
-						alt={recipe.name}
-						src={recipe.imageUrl ?? 'https://placehold.co/400'}
-						class="aspect-square"
-					/>
+					<img alt={recipe.name} src={recipe.imageUrl ?? 'https://placehold.co/400'} />
 
 					<h3 class="text-center font-bold text-base sm:text-lg mt-1">{recipe.name}</h3>
-					<small
-						class="text-neutral-300 text-center w-full font-medium font-sans tracking-wide"
-						>Ingredients</small
+					<small class="text-neutral-300 text-center w-full font-medium font-sans tracking-wide">
+						Ingredients
+					</small>
+					<div
+						class="flex flex-row gap-1 items-center w-full justify-center text-xl px-2 flex-wrap"
 					>
-					<div class="flex flex-row gap-1 items-center w-full justify-center text-xl">
 						{#each getIngredientImages(recipe.ingredients) as ingredientImage}
 							<span>{ingredientImage}</span>
 						{/each}
