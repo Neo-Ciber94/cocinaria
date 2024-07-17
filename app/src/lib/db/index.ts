50aeb64e24a3b0b8de4d35e8b9dbed7534be5731
Freddy L <neo-ciber_94@hotmail.com>
2024-07-17 09:51:10 -0400

    Update database

--- app/src/lib/db/index.ts
+++ app/src/lib/db/index.ts
@@ -1,10 +1,10 @@
 import * as schema from './schema';
-import { neon } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-http';
+import { Pool } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-serverless';
 
 if (!process.env.DATABASE_URL) {
 	throw new Error('"DATABASE_URL" environment variable is not set');
 }
 
-export const client = neon(process.env.DATABASE_URL);
+export const client = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle(client, { schema });
