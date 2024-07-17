1202d1c784cfdcf8b48513904041d815f9d0c8c2
Freddy L <neo-ciber_94@hotmail.com>
2024-07-16 23:11:39 -0400

    minor changes

--- app/src/lib/db/index.ts
+++ app/src/lib/db/index.ts
@@ -1,11 +1,10 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
-import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) {
+if (!process.env.DATABASE_URL) {
 	throw new Error('"DATABASE_URL" environment variable is not set');
 }
 
-export const client = postgres(env.DATABASE_URL);
+export const client = postgres(process.env.DATABASE_URL);
 export const db = drizzle(client, { schema });
