86f36ebb0fd79d7512699e8b72e404479ad37ede
Freddy L <neo-ciber_94@hotmail.com>
2024-07-20 16:30:56 -0400

    refactor auth logic

--- app/src/lib/auth/providers.ts
+++ app/src/lib/auth/providers.ts
@@ -4,16 +4,46 @@ import { getBaseUrl } from '$lib/common/getBaseUrl';
 
 const baseUrl = getBaseUrl();
 
-export const googleAuth = new Google(
-	env.GOOGLE_CLIENT_ID!,
-	env.GOOGLE_CLIENT_SECRET!,
-	`${baseUrl}/api/auth/google/callback`
-);
+export function createGoogleAuth() {
+	if (!env.GOOGLE_CLIENT_ID) {
+		throw new Error('Missing GOOGLE_CLIENT_ID');
+	}
 
-export const githubAuth = new GitHub(env.GITHUB_CLIENT_ID!, env.GITHUB_CLIENT_SECRET!);
+	if (!env.GOOGLE_CLIENT_SECRET) {
+		throw new Error('Missing GOOGLE_CLIENT_SECRET');
+	}
 
-export const discordAuth = new Discord(
-	env.DISCORD_CLIENT_ID!,
-	env.DISCORD_CLIENT_SECRET!,
-	`${baseUrl}/api/auth/discord/callback`
-);
+	return new Google(
+		env.GOOGLE_CLIENT_ID,
+		env.GOOGLE_CLIENT_SECRET,
+		`${baseUrl}/api/auth/google/callback`
+	);
+}
+
+export function createGithubAuth() {
+	if (!env.GITHUB_CLIENT_ID) {
+		throw new Error('Missing GITHUB_CLIENT_ID');
+	}
+
+	if (!env.GITHUB_CLIENT_SECRET) {
+		throw new Error('Missing GITHUB_CLIENT_SECRET');
+	}
+
+	return new GitHub(env.GITHUB_CLIENT_ID, env.GITHUB_CLIENT_SECRET);
+}
+
+export function createDiscordAuth() {
+	if (!env.DISCORD_CLIENT_ID) {
+		throw new Error('Missing DISCORD_CLIENT_ID');
+	}
+
+	if (!env.DISCORD_CLIENT_SECRET) {
+		throw new Error('Missing DISCORD_CLIENT_SECRET');
+	}
+
+	return new Discord(
+		env.DISCORD_CLIENT_ID!,
+		env.DISCORD_CLIENT_SECRET!,
+		`${baseUrl}/api/auth/discord/callback`
+	);
+}
