29980c18ffd5bf563765c8327b4269dc4a337b3d
Freddy L <neo-ciber_94@hotmail.com>
2024-07-21 15:54:19 -0400

    Add method to consume credits

--- app/src/lib/hooks/useAuth.svelte.ts
+++ app/src/lib/hooks/useAuth.svelte.ts
@@ -1,5 +1,9 @@
 import type { Auth } from '$lib/common/types';
 
+type UseAuth = Auth & {
+	consumeCredit: () => void;
+};
+
 let globalAuth = $state<Auth | null | undefined>();
 
 export function setAuth(auth: Auth | null | undefined) {
@@ -7,6 +11,28 @@ export function setAuth(auth: Auth | null | undefined) {
 	return globalAuth;
 }
 
-export function useAuth() {
-	return globalAuth;
+export function useAuth(): UseAuth | null | undefined {
+	const result = $derived.by(() => {
+		if (globalAuth == null) {
+			return globalAuth;
+		}
+
+		return {
+			get account() {
+				return globalAuth!.account;
+			},
+			get user() {
+				return globalAuth!.user;
+			},
+			get session() {
+				return globalAuth!.session;
+			},
+			consumeCredit() {
+				const newCredits = Math.max(0, globalAuth!.account.credits - 1);
+				globalAuth!.account.credits = newCredits;
+			}
+		};
+	});
+
+	return result;
 }
