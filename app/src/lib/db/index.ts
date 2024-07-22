f5b7316a0e4933428b4e14cdc035a642d66666c8
Freddy L <neo-ciber_94@hotmail.com>
2024-07-22 17:24:23 -0400

    minor changes

--- app/src/lib/db/index.ts
+++ app/src/lib/db/index.ts
@@ -1,3 +1,5 @@
 // We are deploying to vercel, so we use a serverless adapter,
 // if you are using a serverfull environment you can use the `adapters/pg` instead
 export * from './adapters/neon';
+
+// export * from './adapters/pg';
