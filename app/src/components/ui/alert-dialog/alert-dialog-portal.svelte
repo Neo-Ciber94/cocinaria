168c7423df6cec028f342fe50fbc2ef4f4965bf0
Freddy L <neo-ciber_94@hotmail.com>
2024-07-21 21:00:15 -0400

    lint

--- app/src/components/ui/alert-dialog/alert-dialog-portal.svelte
+++ app/src/components/ui/alert-dialog/alert-dialog-portal.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	import { AlertDialog as AlertDialogPrimitive } from 'bits-ui';
 
 	type $$Props = AlertDialogPrimitive.PortalProps;
