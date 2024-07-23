7999629c851e5d66f9567d9201d45ba15960c194
Freddy L <neo-ciber_94@hotmail.com>
2024-07-22 21:40:48 -0400

    Update migrate local recipes script

--- app/scripts/migrateLocalRecipesToProd.ts
+++ app/scripts/migrateLocalRecipesToProd.ts
@@ -7,22 +7,45 @@ dotenv.config({
 import * as schema from '../src/lib/db/schema';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
-import type { recipes } from '../src/lib/db/schema';
+import { recipes } from '../src/lib/db/schema';
 import nodeCrypto from 'node:crypto';
+import { CopyObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import path from 'node:path';
+import { generateBase64Id } from '$lib/server/utils';
+
+const s3Client = new S3Client({
+	endpoint: process.env.S3_API_ENDPOINT,
+	credentials: {
+		accessKeyId: process.env.S3_ACCESS_KEY!,
+		secretAccessKey: process.env.S3_ACCESS_SECRET_KEY!
+	}
+});
 
 async function main() {
 	const neonLocal = createPgClient(process.env.DATABASE_LOCAL_URL!);
 	const neonRemote = createPgClient(process.env.DATABASE_REMOTE_URL!);
 
-	const remoteRecipes = await neonRemote.db.query.recipes.findMany({
+	const targetUser = await neonRemote.db.query.users.findFirst({
 		where(fields, { eq }) {
-			return eq(fields.userId, String(process.env.MIGRATE_TO_USER_ID));
+			return eq(fields.id, String(process.env.MIGRATE_TO_USER_ID));
 		}
 	});
 
-	await neonLocal.db.transaction(async (tx) => {
-		const localRecipes = await tx.query.recipes.findMany();
-		console.log(`🕒 Migrating ${localRecipes.length} recipes to remote database...`);
+	if (!targetUser) {
+		throw new Error('User not found');
+	}
+
+	console.log(`Migrating recipe images to user: ${targetUser.id}, User(${targetUser.username})`);
+
+	const localRecipes = await neonLocal.db.query.recipes.findMany();
+	console.log(`🕒 Migrating ${localRecipes.length} recipes to remote database...`);
+
+	await neonRemote.db.transaction(async (tx) => {
+		const remoteRecipes = await tx.query.recipes.findMany({
+			where(fields, { eq }) {
+				return eq(fields.userId, String(process.env.MIGRATE_TO_USER_ID));
+			}
+		});
 
 		for (const recipe of localRecipes) {
 			const recipeHash = createRecipeHash(recipe);
@@ -33,11 +56,58 @@ async function main() {
 				continue;
 			}
 
+			if (recipe.imageUrl == null) {
+				continue;
+			}
+
 			console.log(`Migrating recipe(${recipe.id}): ${recipe.name}`);
+
+			const { newImageUrl } = await copyRecipeImage(recipe.imageUrl);
+
+			// Insert new recipe
+			await tx.insert(recipes).values({
+				name: recipe.name,
+				description: recipe.description,
+				ingredients: recipe.ingredients,
+				prompt: recipe.prompt,
+				recipe: recipe.recipe,
+				recipeType: recipe.recipeType,
+
+				// New user Id
+				userId: targetUser.id,
+
+				// New image url
+				imageUrl: newImageUrl
+			});
 		}
 	});
 
 	await Promise.allSettled([neonRemote.client.end(), neonLocal.client.end()]);
+}
+
+async function copyRecipeImage(imageUrl: string) {
+	const key = imageUrl.replaceAll(process.env.ASSETS_URL!, '');
+	const fileId = generateBase64Id();
+
+	const ext = path.extname(imageUrl);
+	const newKey = `images/${fileId}${ext}`;
+	const newImageUrl = `${process.env.ASSETS_URL}/${key}`;
+
+	// Copy file
+	console.log(`📂 Copying file '${key}' to ${newKey}, new url: ${newImageUrl}`);
+
+	const bucketName = process.env.S3_BUCKET_NAME;
+	await s3Client.send(
+		new CopyObjectCommand({
+			Bucket: bucketName,
+			CopySource: `${bucketName}/${key}`,
+			Key: newKey
+		})
+	);
+
+	return {
+		newImageUrl
+	};
 }
 
 function createPgClient(databaseUrl: string) {
