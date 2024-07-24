2e16e4fb0ad7efa4597e5592755613e5cbedce90
Freddy L <neo-ciber_94@hotmail.com>
2024-07-24 18:45:43 -0400

    minor changes

--- packages/svelte-picture/src/lib/server/index.ts
+++ packages/svelte-picture/src/lib/server/index.ts
@@ -4,7 +4,9 @@ import sharp from 'sharp';
 
 const IS_DEV = process.env.NODE_ENV !== 'production';
 const IMAGE_CACHE = new Map<string, ArrayBuffer>();
-const MAX_IMAGE_WIDTH = 4096;
+
+// This is just a hard limit
+const MAX_IMAGE_WIDTH = 2048;
 
 // Default cache id used it none is specified.
 const CACHE_ID = 'ZDgzNGRjYzQtODE3NC00ZDQ2LTkzYzMtN2I2ZDcyMGEzMjZi';
