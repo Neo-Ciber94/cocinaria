f32dc0ea8988e5cdfc786389e05eba1125d29148
Freddy L <neo-ciber_94@hotmail.com>
2024-07-24 09:13:38 -0400

    fix

--- app/src/routes/api/image/+server.ts
+++ app/src/routes/api/image/+server.ts
@@ -2,7 +2,7 @@ import { env } from '$env/dynamic/private';
 import { createImageOptimizerHandler } from 'svelte-picture/server';
 
 const handler = createImageOptimizerHandler({
-	allowedOrigins: env.ASSETS_URL ? [] : [env.ASSETS_URL]
+	allowedOrigins: env.ASSETS_URL ? [env.ASSETS_URL] : []
 });
 
 export { handler as GET, handler as HEAD };
