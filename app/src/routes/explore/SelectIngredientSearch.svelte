090431e9e5c994f5124147a7b75a926699004fd6
Freddy L <neo-ciber_94@hotmail.com>
2024-07-27 22:01:39 -0400

    format

--- app/src/routes/explore/SelectIngredientSearch.svelte
+++ app/src/routes/explore/SelectIngredientSearch.svelte
@@ -4,6 +4,8 @@
 	import { INGREDIENTS, type Category } from '$lib/common/ingredients';
 	import { cn } from '$lib/utils';
 	import OrangeSlice from '$components/icons/orangeSliceIcon.svelte';
+	import { useMatchQuery, breakpoints } from '$lib/hooks/useMatchQuery.svelte';
+	import * as Drawer from '$components/drawer';
 
 	type Props = {
 		class?: string;
@@ -14,10 +16,34 @@
 	let { selected = $bindable(), onClose, ...rest }: Props = $props();
 	const initialIngredients = INGREDIENTS.map((x) => ({ label: x.value, ...x }));
 	const ingredientGroups = Object.groupBy(initialIngredients, (e) => e.category);
+
+	const isDesktopQuery = useMatchQuery(breakpoints.desktop);
+	let open = $state(false);
 </script>
+
+{#snippet Items()}
+	{#each Object.keys(ingredientGroups) as group}
+		{@const ingredients = ingredientGroups[group as Category] || []}
+
+		<Select.Group>
+			<Select.Label class="capitalize">{group}</Select.Label>
+			{#each ingredients as ingredient}
+				<Select.Item
+					value={ingredient.value}
+					label={ingredient.label}
+					class="flex flex-row gap-2 px-2 py-2 pr-4 text-base data-[selected]:bg-neutral-100"
+				>
+					<span>{ingredient.image}</span>
+					<span>{ingredient.label}</span>
+				</Select.Item>
+			{/each}
+		</Select.Group>
+	{/each}
+{/snippet}
 
 <Select.Root
 	bind:selected
+	bind:open
 	items={initialIngredients}
 	multiple
 	onOpenChange={(isOpen) => {
@@ -30,63 +56,19 @@
 		<OrangeSlice class="mr-[9px] size-6 text-neutral-300" />
 		<Select.Value placeholder="Ingredients" />
 	</Select.Trigger>
-	<Select.Content class="max-h-[500px] overflow-y-auto">
-		{#each Object.keys(ingredientGroups) as group}
-			{@const ingredients = ingredientGroups[group as Category] || []}
+	{#if isDesktopQuery.matches}
+		<Select.Content class="max-h-[500px] overflow-y-auto">
+			{@render Items()}
+		</Select.Content>
+	{:else}
+		<Drawer.Root>
+			<Drawer.Content bind:open>
+				<div class="max-h-[80vh] min-h-[80vh] overflow-y-auto">
+					{@render Items()}
+				</div>
+			</Drawer.Content>
+		</Drawer.Root>
+	{/if}
 
-			<Select.Group>
-				<Select.Label class="capitalize">{group}</Select.Label>
-				{#each ingredients as ingredient}
-					<Select.Item value={ingredient.value} label={ingredient.label}>
-						<span>{ingredient.image}</span>
-						<span>{ingredient.label}</span>
-					</Select.Item>
-				{/each}
-			</Select.Group>
-		{/each}
-	</Select.Content>
 	<Select.Input name="ingredients" />
 </Select.Root>
-
-<!-- <Select.Root
-	bind:selected
-	items={ingredients}
-	multiple
-	onOpenChange={(isOpen) => {
-		if (!isOpen) {
-			onClose?.();
-		}
-	}}
->
-	<Select.Trigger
-		class={cn(
-			'inline-flex h-10 w-[296px] items-center rounded-md border border-neutral-200 bg-white px-[11px] text-sm transition-colors placeholder:text-black/50  focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 focus:ring-offset-white',
-			rest.class
-		)}
-		aria-label="Ingredients"
-	>
-		<OrangeSlice class="mr-[9px] size-6 text-neutral-300" />
-		<Select.Value class="text-sm data-[placeholder]:text-neutral-400" placeholder="Ingredients" />
-		<Icon icon="heroicons:chevron-up-down-16-solid" class="ml-auto size-6 text-neutral-300" />
-	</Select.Trigger>
-	<Select.Content
-		class="w-full max-h-[500px] overflow-y-auto rounded-xl border border-neutral-200 bg-white px-1 py-3 shadow-sm outline-none"
-		transition={flyAndScale}
-		sideOffset={8}
-	>
-		{#each ingredients as ingredient}
-			<Select.Item
-				class="flex gap-2 h-10 w-full select-none items-center rounded-sm py-3 pl-5 pr-1.5 text-sm outline-none transition-all duration-75 data-[highlighted]:bg-neutral-300 capitalize"
-				value={ingredient.value}
-				label={ingredient.label}
-			>
-				<span>{ingredient.image}</span>
-				<span>{ingredient.label}</span>
-				<Select.ItemIndicator class="ml-auto" asChild={false}>
-					<Icon icon="material-symbols:check" />
-				</Select.ItemIndicator>
-			</Select.Item>
-		{/each}
-	</Select.Content>
-	<Select.Input name="ingredients" />
-</Select.Root> -->
