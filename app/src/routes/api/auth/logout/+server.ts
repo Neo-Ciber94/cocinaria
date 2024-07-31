d5ca5eb0b7d7e0c90a807cb1b0ddeb2cda8a7a46
Freddy L <neo-ciber_94@hotmail.com>
2024-07-30 22:23:06 -0400

    delete sessions after logout

--- app/src/routes/api/auth/logout/+server.ts
+++ app/src/routes/api/auth/logout/+server.ts
@@ -3,8 +3,19 @@ import { COOKIE_AI_PROVIDER_KEY } from '$lib/common/constants';
 import type { RequestEvent } from '@sveltejs/kit';
 
 export async function GET(event: RequestEvent) {
+	const sessionId = event.cookies.get(lucia.sessionCookieName);
+
 	event.cookies.delete(lucia.sessionCookieName, { path: '/' });
 	event.cookies.delete(COOKIE_AI_PROVIDER_KEY, { path: '/' });
+
+	if (sessionId) {
+		try {
+			await lucia.invalidateSession(sessionId);
+			await lucia.deleteExpiredSessions();
+		} catch (err) {
+			console.error(err);
+		}
+	}
 
 	return new Response(null, {
 		status: 302,
